f31a4fdc63437c1e09357212cf46705c597edb00
Quitumba Ferreira <culyssandercentro@gmail.com>
2021-02-12 13:51:53 +0100

    ecommerce nodejs and angular

---
+++ backend/src/models/domain/userModel.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const User = (user) => {
+    this.id = user.id,
+    this.username = user.username,
+    this.password = user.password,
+    this.email = user.email,
+    this.fistname = user.fistname,
+    this.lastname = user.lastname,
+    this.age = user.age,
+    this.role = user.role
+}
